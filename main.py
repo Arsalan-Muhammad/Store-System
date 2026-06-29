8d8626a1b34594a5abde9c102cc36a74ceb0e2a7
Arsalan-Muhammad <arsalancoding@gmail.com>
2026-06-29 16:47:03 +0500

    Solve Corner Cases in create product

--- main.py
+++ main.py
@@ -10,10 +10,34 @@ models.Base.metadata.create_all(bind=engine)
 def home():
     return {"Hi!" : "Welcome to Arsalan Cosmetics Store!"}
 
-@app.post("/add-product")
-def add_product(product:schemas.CreateProduct , db : Session = Depends(get_db)):
+@app.post("/add-product", status_code=status.HTTP_201_CREATED)
+def add_product(
+    product: schemas.CreateProduct,
+    db: Session = Depends(get_db)
+):
+
+    existing_product = db.query(models.Products).filter(
+        models.Products.name == product.name
+    ).first()
+
+    if existing_product:
+        existing_product.quantity += product.quantity #pyright:ignore
+
+        db.commit()
+        db.refresh(existing_product)
+
+        return {
+            "message": "Product quantity updated",
+            "product": existing_product
+        }
+
     new_product = models.Products(**product.dict())
-    
+
     db.add(new_product)
     db.commit()
     db.refresh(new_product)
+
+    return {
+        "message": "New product added",
+        "product": new_product
+    }
