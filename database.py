cc1f0663755d0267c188c78cd06a76ddb3f0ebd6
Arsalan-Muhammad <arsalancoding@gmail.com>
2026-06-29 16:13:06 +0500

    Add Create Product

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:arsalan%40512@localhost:5432/products"
+SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:arsalan%40512@localhost:5432/Store_System"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
